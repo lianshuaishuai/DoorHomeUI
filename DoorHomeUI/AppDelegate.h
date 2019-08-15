c72a26da6f96f10b51c7c5e1a80dd28a0bd6c520
连帅帅 <741312975@qq.com>
2019-08-15 10:16:25 +0800

    Initial Commit

---
+++ DoorHomeUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoorHomeUI
+//
+//  Created by 连帅帅 on 2019/8/15.
+//  Copyright © 2019 连帅帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoorHomeUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoorHomeUI
+//
+//  Created by 连帅帅 on 2019/8/15.
+//  Copyright © 2019 连帅帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
